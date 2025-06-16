661ef72accab6f2cb8eb4e18806594b62fc40a4c
mattbtr <m.beiterhhh@googlemail.com>
2025-06-16 10:10:26 +0200

    Untrack files now listed in .gitignore

---
+++ kundendokumentation_builder/lib/features/auth/register_screen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class RegisterScreen extends StatelessWidget {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  final FirebaseAuth auth;
+
+  RegisterScreen({super.key, FirebaseAuth? auth})
+    : auth = auth ?? FirebaseAuth.instance;
+
+  // Registrierungs-Logik (Funktion)
+  void register(BuildContext context) async {
+    try {
+      await auth.createUserWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+      );
+
+      if (!context.mounted) return; // schützt vor ungültigem Kontext
+
+      Navigator.pop(context);
+    } on FirebaseAuthException catch (e) {
+      String message;
+
+      switch (e.code) {
+        case 'email-already-in-use':
+          message = 'E-Mail is already in use.';
+          break;
+        case 'invalid-email':
+          message = 'E-Mail is not valid.';
+          break;
+        case 'weak-password':
+          message = 'password is too weak.';
+          break;
+        default:
+          message = 'Registration failed';
+      }
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text(message)));
+    } catch (e) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text('Unknown exception: $e')));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //
+    return Scaffold(
+      appBar: AppBar(title: const Text('Register')),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(labelText: 'E-Mail'),
+            ),
+            TextField(
+              controller: passwordController,
+              decoration: const InputDecoration(labelText: 'Passowrd'),
+            ),
+
+            const SizedBox(height: 16),
+
+            // Register-Button: Bei Drücken wird Register-Funktion von oben ausgeführt
+            ElevatedButton(
+              onPressed: () => register(context),
+              child: const Text("Register"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- kundendokumentation_builder/lib/main.dart
+++ kundendokumentation_builder/lib/main.dart
@@ -1,6 +1,36 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; // import für Design UI-Toolkit von Flutter (Widgets, Themes etc.)
+import 'package:kundendokumentation_builder/features/auth/auth_logic.dart'; // import der Authentication-Logik (zeigt Login oder HomeScrenn je Auth.status)
+//import 'pages/home_screen.dart';
+//import 'pages/add_notes_screen.dart';
+import 'package:firebase_core/firebase_core.dart'; // Firebase Core Package für Firebase App in Flutter
+//import 'firebase_options.dart';   // enthält Firebase-Prohjekt-Konfiguration
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'firebase_options_env.dart'; // ersetzt firebase_options.dart
+import 'package:logger/logger.dart';
 
-void main() {
+final logger = Logger();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Sorgt dafür, dass alle Widgets und Dienste bevor Firebase initialisiert wird.
+  // Wichtig bei asynchronem Code wie Firebase.initializeApp()
+
+  await dotenv.load(fileName: '.env');
+
+  try {
+    if (Firebase.apps.isEmpty) {
+      await Firebase.initializeApp(
+        // Firebase Projekt initialisieren
+        options: FirebaseOptionsEnv.currentPlatform,
+        // Verwendet die manuelle Konfiguration aus firebase_options_env.dart die die WErte aus .env nimmt
+        // (enthält u. a. API-Key, Projekt-ID etc.)
+      );
+    }
+  } catch (e) {
+    //print('Firebase init error: $e');
+    logger.e('Firebase init error: $e');
+  }
+
   runApp(const MainApp());
 }
 
@@ -9,12 +39,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'Authentification',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.blue,
+      ), // Aktiviert das neuere Material Design 3 (MD3), primäre Theme-Farben
+      home: AuthLogic(), // Startbildschirm ist die Authentifizierung
     );
   }
 }
